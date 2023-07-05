d73f0a25928b81a94d004368eb8668a05d2ddbf8
Amaaz Arshad <AMAAZARSHAD@cckhi.local>
2023-07-05 16:15:07 +0500

    custom exception handler updated

--- src/main/java/com/example/webapis/exception/CustomExceptionHandler.java
+++ src/main/java/com/example/webapis/exception/CustomExceptionHandler.java
@@ -3,6 +3,8 @@ package com.example.webapis.exception;
 import com.example.webapis.model.CustomError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,13 +15,25 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CustomError> handleException(Exception ex) {
+
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // Default status
+
+        // Determine the status based on the exception type
+        if (ex instanceof IllegalArgumentException) {
+            status = HttpStatus.BAD_REQUEST;
+        } else if (ex instanceof BadCredentialsException) {
+            status = HttpStatus.UNAUTHORIZED;
+        } else if (ex instanceof AccessDeniedException) {
+            status = HttpStatus.FORBIDDEN;
+        }
+
         CustomError customError = CustomError.builder()
                 .timestamp(new Date())
-                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-                .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+                .status(status.value())
+                .error(status.getReasonPhrase())
                 .message(ex.getMessage())
                 .build();
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(customError);
+        return ResponseEntity.status(status).body(customError);
     }
 }
